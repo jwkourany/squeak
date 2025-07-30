e414fabf78f5057f1275d332d6e0fc481ed4c6c9
Jad Kourany <jad.kourany@duke.edu>
2025-07-30 14:44:25 -0400

    Initial Cage, Mouse, Mouseline files

---
+++ Mouse.py
@@ -0,0 +1,23 @@
+from datetime import datetime, timedelta
+from typing import Optional
+
+class Mouse:
+    DEFAULT_WEANING_DAYS = 21
+    
+    def __init__(
+        self,
+        mouse_id: str,
+        sex: str,
+        dob: str,
+        genotype: Optional[str] = None,
+        generation: Optional[str] = None,
+        coat_color: Optional[str] = None,
+        notes: Optional[str] = "",
+        wean_date: Optional[str] = None,
+        alive: bool = True,
+        death_date: Optional[str] = None
+    ):
+        self.mouse_id: str = mouse_id,
+        self.sex: str = sex,
+        self.dob: str = dob,
+        self.genotype: Optional
