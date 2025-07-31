9cc2ab6494317e8f9a31261ded3e1a51e6bb3de4
Jad Kourany <jad.kourany@duke.edu>
2025-07-31 03:04:18 -0400

    Weaning changes

--- Mouse.py
+++ Mouse.py
@@ -1,9 +1,7 @@
 from datetime import datetime, timedelta
 from typing import Optional
 
-class Mouse:
-    DEFAULT_WEANING_DAYS = 21
-    
+class Mouse:    
     def __init__(
         self,
         mouse_id: str,
@@ -17,10 +15,19 @@ class Mouse:
         alive: bool = True,
         death_date: Optional[str] = None
     ):
+        
+        # Core info
         self.mouse_id: str = mouse_id
         self.sex: str = sex
-        self.dob: str = dob
+        self.dob: str = dob #format YYYY-MM-DD
+        
+        # Genetics/appearance
         self.genotype: Optional[str] = genotype
+        self.generation: Optional[str] = generation
+        self.coat_color: Optional[str] = coat_color
+        
+        # Notes & status
         self.notes: str = notes
+        self.wean_date: Optional[str] = wean_date
         self.alive: bool = alive
         self.death_date: Optional[str] = death_date
